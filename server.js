2226f635e540bd19b793432f93616116d4496db9
friespotatotissue <your.email@example.com>
2024-12-24 17:28:34 +0300

    Add proper routing and room handling

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const path = require('path');
 // Serve static files
 app.use('/piano', express.static(path.join(__dirname)));
 
+// Root route - redirect to piano
+app.get('/', (req, res) => {
+    res.redirect('/piano');
+});
+
+// Piano routes
+app.get('/piano', (req, res) => {
+    res.redirect('/piano/lobby');
+});
+
+app.get('/piano/:room', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Socket.IO connection handling
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -28,6 +42,18 @@ io.on('connection', (socket) => {
     // Handle chat messages
     socket.on('a', (msg) => {
         io.emit('a', msg);
+    });
+
+    // Handle channel events
+    socket.on('ch', (msg) => {
+        socket.join(msg._id);
+        socket.room = msg._id;
+        socket.broadcast.to(msg._id).emit('ch', {
+            ch: {
+                _id: msg._id,
+                settings: msg.set || {}
+            }
+        });
     });
 });
 
